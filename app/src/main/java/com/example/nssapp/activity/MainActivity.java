34cc844bcc0a4a6af3b992cba5a98fbdaa51f0bb
vineeth-vk11 <k.vineeth2000@gmail.com>
2019-07-20 11:15:00 +0530

    Firebase Login Implemented

--- app/src/main/java/com/example/nssapp/activity/MainActivity.java
+++ app/src/main/java/com/example/nssapp/activity/MainActivity.java
@@ -1,35 +1,83 @@
 package com.example.nssapp.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 import com.example.nssapp.R;
-import com.example.nssapp.activity.HomeActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtEmailAddress;
+    private EditText txtPassword;
+    private FirebaseAuth mAuth;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.textView);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        txtEmailAddress = findViewById(R.id.login_email);
+        txtPassword = findViewById(R.id.login_password);
+        mAuth = FirebaseAuth.getInstance();
+
+        Button button1 = findViewById(R.id.textView2);
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openhomeactivity();
+                openregisteractivity();
             }
         });
 
 
     }
 
-    public void openhomeactivity(){
-        Intent intent = new Intent(this, HomeActivity.class);
+
+
+    public void openregisteractivity(){
+        Intent intent = new Intent(this, register.class);
         startActivity(intent);
+    }
+
+    public void Login(View v){
+        final ProgressDialog progressDialog = ProgressDialog.show(MainActivity.this,"Please wait...","Processing...",true);
+        final String email = txtEmailAddress.getText().toString().trim();
+        String password = txtPassword.getText().toString().trim();
+
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this,getString(R.string.login_success),Toast.LENGTH_LONG).show();
+                            Intent i = new Intent(MainActivity.this,HomeActivity.class);
+                            startActivity(i);
+                        }else {
+                            Toast.makeText(MainActivity.this,getString(R.string.login_fail),Toast.LENGTH_LONG).show();
+
+                        }
+
+                    }
+                });
+
+
+
     }
 }
--- app/src/main/java/com/example/nssapp/activity/aboutclp.java
+++ app/src/main/java/com/example/nssapp/activity/aboutclp.java
@@ -1,6 +1,6 @@
 package com.example.nssapp.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.nssapp.R;
