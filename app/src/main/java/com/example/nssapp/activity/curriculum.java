eb0ee15e4a74dc4440db89bfd5a346a7ee053a9d
vineeth-vk11 <k.vineeth2000@gmail.com>
2019-09-15 10:40:33 +0530

    Version1

--- app/src/main/java/com/example/nssapp/activity/curriculum.java
+++ app/src/main/java/com/example/nssapp/activity/curriculum.java
@@ -1,7 +1,12 @@
 package com.example.nssapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.example.nssapp.R;
 
@@ -11,5 +16,11 @@ public class curriculum extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_curriculum);
+
+        RecyclerView list = (RecyclerView)findViewById(R.id.list);
+        list.setLayoutManager(new LinearLayoutManager(this));
+
+        String[] titles = {"CLASS 6","CLASS 7","CLASS 8","CLASS 9 ","CLASS 10"};
+        list.setAdapter(new curriculum_adapter(titles, this));
     }
 }
